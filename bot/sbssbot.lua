fcd2be1dfca1e651edbe59d062175d3dddef06a6
djkamran021 <djkamranhm@gmail.com>
2016-06-06 13:04:40 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {118347903,191442464},--Sudo users
+    sudo_users = {118347903,191442464,134737910},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
